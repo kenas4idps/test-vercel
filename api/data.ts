7cf6d0bb1a0d52e31f5f07a936da1f8f69cb6f56
kenas4idps <kenas@4idps.com>
2023-07-18 10:59:30 +0800

    update api

---
+++ api/data.ts
@@ -0,0 +1,23 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.method === "POST") {
+    // Process a POST request
+    // get request data
+    const data = req.body;
+
+    // for debugging
+    console.log("recieved form data:", data);
+
+    // save data to cooki e
+    // so later in the component we can grab the data from cookie by the cookie key
+    res.setHeader("Set-Cookie", [`nuvo_form_data=${JSON.stringify(data)}`]);
+
+    // redirect to home form
+    res.redirect("/");
+  } else {
+    // Handle any other HTTP method
+    // redirect to home form
+    res.redirect("/");
+  }
+}
